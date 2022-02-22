a7528499d1d487cf4bbd5e13c6899d17998d770c
unknown <https://github.com/Tanzeena/batch-18.git>
2022-02-22 12:36:06 +0600

    practice

---
+++ tanzeena/aleena/practise 2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=H, initial-scale=1.0">
+    <title>php Document</title>
+</head>
+<body>
+    <?php echo 'Hello EVERYONE';
+  
+       echo  'I am a home maker';
+       ?>
+
+</body>
+</html>
