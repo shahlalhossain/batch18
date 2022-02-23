92c6a46634edefb8a9ba112398e1154fcee2b880
unknown <https://github.com/Tanzeena/batch-18.git>
2022-02-23 20:53:38 +0600

    tanzeena

--- tanzeena/aleena/practise 2.php
+++ tanzeena/aleena/practise 2.php
@@ -9,8 +9,9 @@
 <body>
     <?php echo 'Hello EVERYONE';
   
-       echo  'I am a home maker';
-       ?>
-
+       echo  'For military purposes, Bangladesh was divided into 11 sectors under 11 sector commanders';
+       echo 'In addition to these sectors, later in the war, three special forces were formed: Z Force, S Force and K Force.'
+        ; ?>
+     
 </body>
 </html>
